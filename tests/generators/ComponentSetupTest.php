373b48ffc3946d3647aea2601656d4ad4b7e98a7
mitchell stanley <ubuntumitch@gmail.com>
2014-06-29 12:50:05 +0100

    test alternate path for component setup

--- tests/generators/ComponentSetupTest.php
+++ tests/generators/ComponentSetupTest.php
@@ -72,8 +72,6 @@ class ComponentSetupTest extends PHPUnit_Framework_TestCase {
 	public function can_change_framework()
 	{
 		$file = m::mock('Illuminate\Filesystem\Filesystem[put,exists,isDirectory,makeDirectory,copy]');
-		$original = 'vendor/acoustep/component-generator/src/config/config.php';
-		$target = 'app/config/packages/acoustep/component-generator/config.php';
 
 		$file->shouldReceive('exists')
 			->with(__DIR__.'/stubs/config.php')
@@ -95,6 +93,46 @@ class ComponentSetupTest extends PHPUnit_Framework_TestCase {
 			true
 		);
 
+	}
+
+	/**
+	 * @test
+	 */
+	public function can_publish_config_file()
+	{
+		$file = m::mock('Illuminate\Filesystem\Filesystem[put,exists,isDirectory,makeDirectory,copy]');
+
+		$file->shouldReceive('exists')
+			->with(__DIR__.'/stubs/config.php')
+			->andReturn(false);
+
+		$file->shouldReceive('isDirectory')
+			->with(__DIR__.'/stubs')
+			->andReturn(false);
+
+		$file->shouldReceive('makeDirectory')
+			->with(__DIR__.'/stubs', 0777, true)
+			->andReturn(true);
+
+		$file->shouldReceive('copy')
+			->with("/var/www/laravel.dev/workbench/acoustep/component-generator/src/Acoustep/ComponentGenerator/Generators/../../../config/config.php", __DIR__.'/stubs/config.php')
+			->andReturn(true);
+
+		$file->shouldReceive('put')
+			->once()
+			->with(
+				__DIR__.'/stubs/config.php', 
+				file_get_contents(__DIR__.'/stubs/config.txt'
+			));
+
+		$config = m::mock('ConfigMock');
+		$generator = new ComponentSetup($file, $config, __DIR__, '/stubs');
+		$result = $generator->make('blade', 'app/views/components', 'bootstrap3');
+
+		$this->assertEquals(
+			$result,
+			true
+		);
 	}
 }
 
